4aa14dbd385334493edf274fac84d16c7dd55b73
amohan <amohan@amohanmacpro.local>
2021-05-06 08:03:09 -0400

    updated week11 materials

---
+++ Week11/src-code/queue/driver.py
@@ -0,0 +1,10 @@
+from queue import queue
+q = queue();
+for no in range(0, 10):
+	q.enqueue(no)
+	q.display()
+'''
+for no in range(0, 11):
+	q.dequeue()
+	q.display()
+'''
---
+++ Week11/src-code/stack/driver.py
@@ -0,0 +1,10 @@
+from stack import stack
+s = stack();
+for no in range(0, 10):
+	s.push(no)
+s.display()
+'''
+for no in range(0, 11):
+	s.pop()
+	s.display()
+'''
